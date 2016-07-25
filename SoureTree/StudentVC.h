6fb163e6d4d0a571fe80d8b9da9a504b1639b76b
lichen <15960213872@163.com>
2016-07-25 18:24:55 +0800

    add studentVC to project !!

---
+++ SoureTree/StudentVC.h
@@ -0,0 +1,13 @@
+//
+//  StudentVC.h
+//  SoureTree
+//
+//  Created by  lichen on 16/7/25.
+//  Copyright © 2016年  lichen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentVC : UIViewController
+
+@end
---
+++ SoureTree/StudentVC.m
@@ -0,0 +1,42 @@
+//
+//  StudentVC.m
+//  SoureTree
+//
+//  Created by  lichen on 16/7/25.
+//  Copyright © 2016年  lichen. All rights reserved.
+//
+
+#import "StudentVC.h"
+
+@interface StudentVC ()
+
+@end
+
+@implementation StudentVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)sayBye
+{
+    NSLog(@"bye bye world !!!");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
